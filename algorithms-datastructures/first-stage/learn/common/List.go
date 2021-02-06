ed4a4b1a4d43044001f50932ab7efd4739973d94
wjj <wangzhe2@pconline.com.cn>
2021-02-07 02:52:17 +0800

    出错了怎么办

--- algorithms-datastructures/first-stage/learn/common/List.go
+++ algorithms-datastructures/first-stage/learn/common/List.go
@@ -1,5 +1,10 @@
 package common
 
+import (
+	"fmt"
+	"strconv"
+)
+
 type List interface {
 	Add(element interface{})
 	AddWithIndex(index int, element interface{})
@@ -11,10 +16,33 @@ type List interface {
 	Contains(element interface{}) bool
 	Get(index int) interface{}
 	Set(index int, element interface{}) interface{}
-	IndexOf(element interface{})
+	IndexOf(element interface{}) int
 }
 
 type AbstractList struct {
-	 ElementNotFount int
-	 size int
+	ElementNotFount int
+	size            int
+}
+
+func (al *AbstractList) Size() int {
+	return al.size
+}
+func (al *AbstractList) IsEmpty() bool {
+	return al.size == 0
+}
+
+func (al *AbstractList) outOfBound(index int) {
+	fmt.Println("Size: " + strconv.Itoa(al.size) + " Index: " + strconv.Itoa(index))
+}
+func (al *AbstractList) rangeCheck(index int) {
+	if index < 0 || index >= al.size {
+		al.outOfBound(index)
+	}
+}
+func (al *AbstractList) rangeCheckForAdd(index int) bool {
+	if index < 0 || index > al.size {
+		al.outOfBound(index)
+	} else {
+		return true
+	}
 }
