35985e22192c4e5a975ce93fb4fe362308b4dd73
wjj <wangzhe2@pconline.com.cn>
2021-02-25 02:14:46 +0800

    go可变长数组

--- algorithms-datastructures/first-stage/learn/common/List.go
+++ algorithms-datastructures/first-stage/learn/common/List.go
@@ -1,48 +1,100 @@
 package common
 
 import (
+	"errors"
 	"fmt"
 	"strconv"
 )
 
-type List interface {
-	Add(element interface{})
-	AddWithIndex(index int, element interface{})
-	Remove(index int) interface{}
-	RemoveElement(element interface{})
-	Clear()
-	Size() int
-	IsEmpty() bool
-	Contains(element interface{}) bool
-	Get(index int) interface{}
-	Set(index int, element interface{}) interface{}
-	IndexOf(element interface{}) int
-}
+//type List interface {
+//	Add(element interface{})
+//	AddWithIndex(index int, element interface{})
+//	Remove(index int) interface{}
+//	RemoveElement(element interface{})
+//	Clear()
+//	Size() int
+//	IsEmpty() bool
+//	Contains(element interface{}) bool
+//	Get(index int) interface{}
+//	Set(index int, element interface{}) interface{}
+//	IndexOf(element interface{}) int
+//}
+
+const (
+	elementNotFound = -1
+	defaultCapacity = 10
+)
 
 type AbstractList struct {
-	ElementNotFount int
-	size            int
+	size         int
+	AddWithIndex func(index int, element int)
+	Remove       func(index int) int
+	Clear        func()
+	Get          func(index int) int
+	Set          func(index int, element int) int
+	IndexOf      func(element int) int
+}
+
+func (al *AbstractList) Add(element int) {
+	al.AddWithIndex(al.size, element)
 }
 
 func (al *AbstractList) Size() int {
 	return al.size
 }
+
 func (al *AbstractList) IsEmpty() bool {
 	return al.size == 0
 }
+func (al *AbstractList) Contains(element int) bool {
+	return al.IndexOf(element) != elementNotFound
+}
+func (al *AbstractList) RemoveElement(element int) {
+	al.Remove(al.IndexOf(element))
+}
 
-func (al *AbstractList) outOfBound(index int) {
-	fmt.Println("Size: " + strconv.Itoa(al.size) + " Index: " + strconv.Itoa(index))
-}
-func (al *AbstractList) rangeCheck(index int) {
+func (al *AbstractList) rangeCheck(index int) error {
 	if index < 0 || index >= al.size {
-		al.outOfBound(index)
+		return errors.New("Size: " + strconv.Itoa(al.size) + " Index: " + strconv.Itoa(index))
 	}
+	return nil
 }
-func (al *AbstractList) rangeCheckForAdd(index int) bool {
-	if index < 0 || index > al.size {
-		al.outOfBound(index)
-	} else {
-		return true
-	}
+
+type ArrayList struct {
+	AbstractList
+	elements []int
+}
+
+func NewArrayListWithCapacity(capacity int) *ArrayList {
+	arrayList := new(ArrayList)
+	arrayList.elements = [10]int
+	arrayList.AbstractList.AddWithIndex = arrayList.AddWithIndex
+	arrayList.AbstractList.Remove = arrayList.Remove
+	arrayList.AbstractList.Clear = arrayList.Clear
+	arrayList.AbstractList.Get = arrayList.Get
+	arrayList.AbstractList.Set = arrayList.Set
+	arrayList.AbstractList.IndexOf = arrayList.IndexOf
+	return arrayList
+}
+func NewArrayList() *ArrayList {
+	return NewArrayListWithCapacity(defaultCapacity)
+}
+func (arrayList *ArrayList) AddWithIndex(index int, element int) {
+
+}
+
+func (arrayList *ArrayList) Remove(index int) int {
+
+}
+func (arrayList *ArrayList) Clear() {
+
+}
+func (arrayList *ArrayList) Get(index int) int {
+
+}
+func (arrayList *ArrayList) Set(index int, element int) int {
+
+}
+func (arrayList *ArrayList) IndexOf(element int) int {
+
 }
