3e8c0f7b157a5f56cc5982517f596d9cf4c13d11
wzzst310 <wzzst310@163.com>
2021-02-27 14:12:17 +0800

    arraylist

--- algorithms-datastructures/first-stage/learn/common/list_test.go
+++ algorithms-datastructures/first-stage/learn/common/list_test.go
@@ -11,4 +11,10 @@ func TestArrayList(t *testing.T) {
 		list.Add(i)
 	}
 	fmt.Println(list)
+	fmt.Println(list.Get(2))
+	fmt.Println(list.Contains(2))
+	fmt.Println(list.Remove(2))
+	fmt.Println(list.Contains(2))
+	list.Clear()
+	fmt.Println(list)
 }
