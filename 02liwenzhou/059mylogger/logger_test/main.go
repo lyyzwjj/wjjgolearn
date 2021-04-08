9ba8a57699786e36e2e54bd3b9dfb030a517cab3
wzzst310 <39961347+wzzst310@users.noreply.github.com>
2021-04-09 03:44:37 +0800

    日志库完整实现

--- 02liwenzhou/059mylogger/logger_test/main.go
+++ 02liwenzhou/059mylogger/logger_test/main.go
@@ -1,14 +1,16 @@
 package main
 
 import (
-	"time"
 	"wjjgolearn/02liwenzhou/059mylogger/mylogger"
 )
 
+var log mylogger.Logger // 声明一个全局的全局变量
+
 // 测试我们自己写的日志库
 func main() {
-	// log := mylogger.NewConsoleLogger("INFO")
-	log := mylogger.NewFileLogger("INFO", "./", "customer", 10*1024*1024)
+	log = mylogger.NewConsoleLogger("INFO")                         //  终端日志实例
+	log = mylogger.NewFileLogger("INFO", "./", "customer", 10*1024) //  文件日志实例
+	// log = mylogger.NewLogger("f")                                   //根据入参返回不同的Logger
 	for {
 		log.Debug("这是一条debug日志")
 		log.Info("这是一条info日志")
@@ -17,6 +19,6 @@ func main() {
 		name := "理想"
 		log.Error("这是一条error日志,id:%d, name:%s", id, name)
 		log.Fatal("这是一条fatal日志")
-		time.Sleep(2 * time.Second)
+		// time.Sleep(2 * time.Second)
 	}
 }
---
+++ 02liwenzhou/061file_demo/main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+// 1. 文件对象的类型
+// 2. 获取文件对象的详细信息
+
+func main() {
+	fileObj, err := os.Open("./main.go")
+	if err != nil {
+		fmt.Printf("open file error failed,err:%v\n", err)
+		return
+	}
+	// 1. 文件对象的类型
+	fmt.Printf("%T\n", fileObj)
+	// 2. 获取文件对象的详细信息
+	fileInfo, err := fileObj.Stat()
+	if err != nil {
+		fmt.Printf("get file info failed,err:%v\n", err)
+		return
+	}
+	fmt.Printf("文件的大小是：%dB\n", fileInfo.Size())
+	fmt.Printf("文件的名字是：%s\n", fileInfo.Name())
+	fmt.Println(fileInfo)
+
+}
