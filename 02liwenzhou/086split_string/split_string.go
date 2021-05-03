24fb5cf5dfde8a2805859b22b5664a7eec374c00
wzzst310 <wzzst310@163.com>
2021-05-03 08:25:29 +0800

    golang 测试 ResetTimer Setup Teardown Example

--- 02liwenzhou/086split_string/split_string.go
+++ 02liwenzhou/086split_string/split_string.go
@@ -11,10 +11,12 @@ import (
 
 func Split(str string, sep string) []string {
 	// str:"babcbef" sep="b" [a cdef]
-	var ret []string
+	// var ret []string
+	// 优化  直接算好最终的[]string 的长度
+	var ret = make([]string, 0, strings.Count(str, sep)+1)
 	index := strings.Index(str, sep)
 	for index >= 0 {
-		ret = append(ret, str[0:index])
+		ret = append(ret, str[0:index]) //append会帮助初始化 此处申请分配内存 避免运行时多次分配内存
 		str = str[index+len(sep):]
 		index = strings.Index(str, sep)
 	}
@@ -22,6 +24,14 @@ func Split(str string, sep string) []string {
 	if index == -5 {
 		fmt.Println("真无聊")
 	}
+	// time.Sleep(time.Second) // 基准测试执行1s  睡1s刚好只执行一次
 	ret = append(ret, str)
 	return ret
+}
+
+func Fib(n int) int {
+	if n < 2 {
+		return n
+	}
+	return Fib(n-1) + Fib(n-2)
 }
--- 02liwenzhou/086split_string/split_string_test.go
+++ 02liwenzhou/086split_string/split_string_test.go
@@ -1,6 +1,8 @@
 package _86split_string
 
 import (
+	"fmt"
+	"os"
 	"reflect"
 	"testing"
 )
@@ -17,6 +19,86 @@ import (
 // go test -cover -coverprofile=cover.out
 // go tool cover -html=cover.out
 // 绿色 覆盖到的 红色 未覆盖到的
+
+// 基准测试
+// go test -bench=. 运行所有的
+// go test -bench=Split   (函数名 Split)
+// -benchmem 查看每次操作内存情况 go test -bench=Split -benchmem
+
+// 默认情况下 每个基准测试最少运行1秒.如果在Benchmark函数返回时没有到1秒,则b.N的值就会按1,2,5,10,20,50,...增加,并且函数再次运行
+func BenchmarkSplit(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		Split("a:b:c:d:e", ":")
+	}
+}
+
+/*
+wjj@wjjdeMacBook-Pro 086split_string % go test -bench=Split
+goos: darwin
+goarch: amd64
+pkg: wjjgolearn/02liwenzhou/086split_string
+cpu: Intel(R) Core(TM) i5-8250U CPU @ 1.60GHz
+BenchmarkSplit-8         2978284               397.4 ns/op
+PASS
+ok      wjjgolearn/02liwenzhou/086split_string  1.604s
+*/
+/*
+BenchmarkSplit-8    8表示GOMAXPROCS 核心数  397.4 ns/op 每次操作耗费秒数
+*/
+
+/*
+wjj@wjjdeMacBook-Pro 086split_string % go test -bench=Split -benchmem
+goos: darwin
+goarch: amd64
+pkg: wjjgolearn/02liwenzhou/086split_string
+cpu: Intel(R) Core(TM) i5-8250U CPU @ 1.60GHz
+BenchmarkSplit-8         2634758               430.2 ns/op           240 B/op          4 allocs/op
+PASS
+ok      wjjgolearn/02liwenzhou/086split_string  1.607s
+*/
+
+/*
+240 B/op   	每次申请内存大小
+4 allocs/op	申请内存次数
+*/
+
+/* 内存优化完之后
+wjj@wjjdeMacBook-Pro 086split_string % go test -bench=Split -benchmem
+goos: darwin
+goarch: amd64
+pkg: wjjgolearn/02liwenzhou/086split_string
+cpu: Intel(R) Core(TM) i5-8250U CPU @ 1.60GHz
+BenchmarkSplit-8         7367452               175.0 ns/op            80 B/op          1 allocs/op
+PASS
+ok      wjjgolearn/02liwenzhou/086split_string  1.466s
+*/
+
+func benchmarkFib(b *testing.B, n int) {
+	for i := 0; i < b.N; i++ {
+		Fib(n)
+	}
+}
+
+// go test -bench=Fib2
+func BenchmarkFib1(b *testing.B) {
+	// benchmarkFib(b, b.N) 绝对不允许 用b.N随便当参数
+	benchmarkFib(b, 1)
+}
+func BenchmarkFib2(b *testing.B) {
+	benchmarkFib(b, 2)
+}
+func BenchmarkFib3(b *testing.B) {
+	benchmarkFib(b, 3)
+}
+func BenchmarkFib10(b *testing.B) {
+	benchmarkFib(b, 10)
+}
+func BenchmarkFib20(b *testing.B) {
+	benchmarkFib(b, 20)
+}
+func BenchmarkFib40(b *testing.B) {
+	benchmarkFib(b, 40)
+}
 
 func TestSplit(t *testing.T) {
 	// 利用结构体包装测试参数
@@ -50,6 +132,9 @@ func TestSplit(t *testing.T) {
 		"case_3": {"abcef", "bc", []string{"a", "ef"}},
 		"case_4": {"沙河有沙又有河", "有", []string{"沙河", "沙又", "河"}},
 	}
+
+	teardownTestCase := setupTestCase(t) // 测试之前执行setup操作
+	defer teardownTestCase(t)            // 测试之后执行testdoen操作
 	for name, tc := range testGroup {
 		t.Run(name, func(t *testing.T) {
 			got := Split(tc.str, tc.sep)
@@ -91,3 +176,56 @@ func Test3Split(t *testing.T) {
 	fmt.Printf("%#v\n", ret)
 }
 */
+
+// 重置时间 b.ResetTimer() 有些准备工作或其他的不应该被统计 见 gba_benchmark
+
+// 并行测试  有使用goroutine的情况下测试
+// -cpu 指定使用的CPU数量
+// go test -bench=. -cpu 1
+// go test -bench=Split -cpu 1
+func BenchmarkSplitParallel(b *testing.B) {
+	// b.SetParallelism(1) // 设置使用的CPU数
+	b.RunParallel(func(pb *testing.PB) {
+		for pb.Next() {
+			Split("沙河有沙又有河", "沙")
+		}
+	})
+}
+
+// Setup与TearDown
+func TestMain(m *testing.M) {
+	fmt.Println("write setup code here...") // 测试之前的做一些设置
+	// 如果 TestMain 使用了 flags，这里应该加上flag.Parse()
+	retCode := m.Run()                         // 执行测试
+	fmt.Println("write teardown code here...") // 测试之后做一些拆卸工作
+	os.Exit(retCode)                           // 退出测试
+}
+
+// 测试集的Setup与Teardown
+func setupTestCase(t *testing.T) func(t *testing.T) {
+	t.Log("如有需要在此执行:测试之前的setup")
+	return func(t *testing.T) {
+		t.Log("如有需要在此执行:测试之后的teardown")
+	}
+}
+
+// 子测试的Setup与Teardown
+func setupSubTest(t *testing.T) func(t *testing.T) {
+	t.Log("如有需要在此执行:子测试之前的setup")
+	return func(t *testing.T) {
+		t.Log("如有需要在此执行:子测试之后的teardown")
+	}
+}
+
+// 示例函数 以Example为前缀 利用 go dock 工具
+// 三大好处
+// 1. 示例函数能够作为文档直接使用，例如基于web的godoc中能把示例函数与对应的函数或包相关联。
+// 2. 示例函数只要包含了// Output:也是可以通过go test运行的可执行测试。  go test -run Example
+// 3. 示例函数提供了可以直接运行的示例代码，可以直接在golang.org的godoc文档服务器上使用Go Playground运行示例代码。下图为strings.ToUpper函数在Playground的示例函数效果。
+func ExampleSplit() {
+	fmt.Println(Split("a:b:c", ":"))
+	fmt.Println(Split("沙河有沙又有河", "沙"))
+	// Output:
+	// [a b c]
+	// [ 河有 又有河]
+}
