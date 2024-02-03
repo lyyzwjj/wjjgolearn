1159e41805c1db31a6a2302728bb3546adef2f59
lyyzwjj <lyyzwjj@gmail.com>
2024-02-03 23:51:05 +0800

    测试

--- 00myself/_006private_repository/go.mod
+++ 00myself/_006private_repository/go.mod
@@ -1,3 +1,5 @@
 module private_repository
 
 go 1.21.6
+
+require gitee.com/lyyzwjj/wjjgolearn v4.0.1-0.20240203151532-7cb8b5ad13cb+incompatible // indirect
--- 00myself/_006private_repository/main.go
+++ 00myself/_006private_repository/main.go
@@ -1,1 +1,11 @@
-package _006private_repository
+package main
+
+import (
+	"gitee.com/lyyzwjj/wjjgolearn/private_repository/gitee"
+)
+
+func main() {
+	gitee.FunTest()
+	gitee.FunParamsTest("a")
+	println("TestParam")
+}
